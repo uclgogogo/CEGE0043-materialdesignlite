34dc5668f757e21dd0c6a00339e1465e86cceb8b
uclgogogo <logan1234456@outlook.com>
2019-02-21 11:41:14 +0000

    2b ajax test

--- ucfnglo/www/js/AJAX.js
+++ ucfnglo/www/js/AJAX.js
@@ -28,7 +28,7 @@ function callDivNodeJSChange() {
     try {
         xhrNode.setRequestHeader("Content-Type", "application/x-www-formurlencoded");
     } catch (e) {
-    // this only works in internet explorer
+        // this only works in internet explorer
     }
     xhrNode.send();
 }
@@ -37,7 +37,8 @@ function processDivNodeJSChange() {
     if (xhrNode.readyState < 4) // while waiting response from server
         document.getElementById('ajaxtext').innerHTML = "Loading...";
     else if (xhrNode.readyState === 4) { // 4 = Response from server has been completely
-        loaded.if(xhrNode.status == 200 && xhrNode.status < 300)
+        loaded.if(xhrNode.status == 200 && xhrNode.status < 300);
         // http status between 200 to 299 are all successful
         document.getElementById('ajaxtext').innerHTML = xhrNode.responseText;
     }
+}
