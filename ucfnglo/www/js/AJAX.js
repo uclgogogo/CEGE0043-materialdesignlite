8eef694d38c31beda57a21c53a5f4a07176ff47d
uclgogogo <logan1234456@outlook.com>
2019-02-21 11:39:50 +0000

    2b ajax test

--- ucfnglo/www/js/AJAX.js
+++ ucfnglo/www/js/AJAX.js
@@ -1,4 +1,5 @@
 var xhr; // define the global variable to process the AJAX request
+var xhrNode;
 function getHTML() {
     xhr = new XMLHttpRequest();
     var filename = document.getElementById("filename").value;
@@ -18,3 +19,25 @@ function processDivChange() {
             document.getElementById('div1').innerHTML = xhr.responseText;
     }
 }
+
+function callDivNodeJSChange() {
+    xhrNode = new XMLHttpRequest();
+    var url = "http://developer.cege.ucl.ac.uk:" + httpPortNumber;
+    xhrNode.open("GET", url, true);
+    xhrNode.onreadystatechange = processDivNodeJSChange;
+    try {
+        xhrNode.setRequestHeader("Content-Type", "application/x-www-formurlencoded");
+    } catch (e) {
+    // this only works in internet explorer
+    }
+    xhrNode.send();
+}
+
+function processDivNodeJSChange() {
+    if (xhrNode.readyState < 4) // while waiting response from server
+        document.getElementById('ajaxtext').innerHTML = "Loading...";
+    else if (xhrNode.readyState === 4) { // 4 = Response from server has been completely
+        loaded.if(xhrNode.status == 200 && xhrNode.status < 300)
+        // http status between 200 to 299 are all successful
+        document.getElementById('ajaxtext').innerHTML = xhrNode.responseText;
+    }
